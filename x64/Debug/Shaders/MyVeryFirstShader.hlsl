bc87385aaf378c586f8f0dd0d7824a250a1fec73
LazyM0b <105501399+LazyM0b@users.noreply.github.com>
2025-03-13 08:09:53 +0300

    added assimp dll files

---
+++ x64/Debug/Shaders/MyVeryFirstShader.hlsl
@@ -0,0 +1,41 @@
+struct VS_IN
+{
+	float4 pos : POSITION0;
+	float4 col : COLOR0;
+};
+
+struct PS_IN
+{
+	float4 pos : SV_POSITION;
+ 	float4 col : COLOR;
+};
+
+cbuffer PositionW : register(b0)
+{
+	float4x4 posW;
+};
+
+cbuffer PositionL : register(b1)
+{
+	float4x4 posL;
+}
+
+PS_IN VSMain( VS_IN input)
+{
+	PS_IN output = (PS_IN)0;
+
+	//float4x4 posM = mul(posW, posL);
+	output.pos = mul(input.pos, posL);
+	output.col = input.col;
+	
+	return output;
+}
+
+float4 PSMain( PS_IN input ) : SV_Target
+{
+	float4 col = input.col;
+//#ifdef TEST
+//	if (input.pos.x > 400) col = TCOLOR;
+//#endif
+	return col;
+}
